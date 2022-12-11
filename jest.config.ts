ceceddbfc5752cff01bfe0822ccd181eb2c721ea
dibo <sherif.eldeeb@learnovia.com>
2022-12-11 17:27:49 +0200

    fix

--- jest.config.ts
+++ jest.config.ts
@@ -3,16 +3,32 @@ import type { Config } from "@jest/types";
 let config: Config.InitialOptions = {
   rootDir: __dirname,
   preset: "ts-jest/presets/default-esm",
-  transform: {
-    "\\.ts$": ["ts-jest", { useESM: true }],
-  },
-  extensionsToTreatAsEsm: [".ts"],
   testEnvironment: "node",
   injectGlobals: false,
   onlyChanged: false,
   collectCoverage: false,
-  moduleDirectories: ["node_modules"],
-  moduleFileExtensions: ["ts", "js"],
+  moduleDirectories: ["node_modules", "types"],
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "mts",
+    "cts",
+    // "mjs",
+    "cjs",
+    "js",
+    "jsx",
+    "json",
+    "html",
+    "scss",
+    "css",
+    "node",
+  ],
+  transform: {
+    "^.+\\.m?[tj]sx?$": ["ts-jest", { useESM: true }],
+  },
+  moduleNameMapper: {},
+  modulePathIgnorePatterns: ["dist"],
+  extensionsToTreatAsEsm: [".ts"],
 };
 
 export default config;
